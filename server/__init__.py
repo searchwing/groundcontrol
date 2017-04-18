de42d8c7d3b000d6a6ebecd1a8ea8d46c479f34c
sascha <sascha@searchwing.org>
2017-04-18 10:56:13 +0200

    incremental improvments in connection management

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,7 @@ Init flask
 (Re)connect UAV in threaded loop
 @author sascha@searchwing.org
 """
-import socket, time, threading
+import socket, time, threading, exceptions
 from datetime import datetime
 
 import flask, dronekit
@@ -31,9 +31,10 @@ app = flask.Flask('searchwing groundcontrol')
 
 
 
-UAV_ADDRESS           = 'tcp:127.0.0.1:5760'
-UAV_RECONNECT_TIMEOUT = 2 # secons
-UAV_CONNECTED_TIMEOUT = 5 # seconds
+UAV_ADDRESS  = 'tcp:127.0.0.1:5760'
+UAV_HEARTBEAT_TIMEOUT       = 20 # seconds
+UAV_CONNECTION_TIMEOUT      =  5 # seconds
+UAV_CONNECTION_TEST_TIMEOUT =  5 # seconds
 
 
 # (Re)connect uav in threaded loop
@@ -43,42 +44,65 @@ class _UAV(threading.Thread):
 
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
+        self.daemon = True
+
         self.uav = None
         self.last_heartbeat = 0
 
-        self.daemon = True
-        self.start()
-
+        self.states = {}
 
     def run(self):
         while 1:
-            if time.time() - self.last_heartbeat > UAV_CONNECTED_TIMEOUT:
+            if time.time() - self.last_heartbeat > UAV_CONNECTION_TIMEOUT:
                 if self.uav:
                     print 'Connection lost UAV %s' % UAV_ADDRESS
-                self.uav = None
+                    self.uav.close()
+                    self.uav = None
 
             if not self.uav:
-                print 'Connecting UAV %s' % UAV_ADDRESS
                 try:
+                    print 'Connecting UAV %s' % UAV_ADDRESS
                     self.uav = dronekit.connect(
-                            UAV_ADDRESS, wait_ready = True, rate = 10)
+                            UAV_ADDRESS,
+                            wait_ready = True,
+                            heartbeat_timeout = UAV_HEARTBEAT_TIMEOUT)
+
+                except socket.error:
+                    print 'error: no server'
+
+                except exceptions.OSError:
+                    print 'error: no serial'
+
+                except dronekit.APIException:
+                    print 'error: timeout'
+
                 except Exception, e:
+                    print 'error: unkown', e
+
+                if not self.uav:
                     print 'Connecting failed UAV %s' % UAV_ADDRESS
 
-                if self.uav:
+                else:
                     print 'Connected UAV %s' % UAV_ADDRESS
-                    last_heartbeat = time.time()
+                    self.last_heartbeat = time.time()
                     self.uav.add_attribute_listener(
                             'last_heartbeat', self.on_heartbeat)
 
-            time.sleep(UAV_RECONNECT_TIMEOUT)
+                    self.uav.add_attribute_listener(
+                            '*', self.on_any)
+
+            time.sleep(UAV_CONNECTION_TEST_TIMEOUT)
 
 
     def on_heartbeat(self, *args, **kwargs):
         self.last_heartbeat = time.time()
 
+    def on_any(self, uav, name, value):
+        self.states[name] = value
+
 
 _uav = _UAV()
+_uav.start()
 
 def get_uav():
     """Get the connected UAV or None.
--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from flask import render_template
 
+# lazy uav resolver
+from . import get_uav
 
 # init and start flask
 from . import app
@@ -10,12 +12,18 @@ from . import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    uav = get_uav()
+    ctx = {
+    }
+    return render_template('index.html', **ctx)
 
 
 @app.route('/map')
 def map():
-    return render_template('map.html')
+    uav = get_uav()
+    ctx = {
+    }
+    return render_template('map.html', **ctx)
 
 
 
