2c4298f84c50e344e416178222355cb88f2f80f0
sascha <sascha@elektropost.org>
2017-06-25 11:06:47 +0200

    st/ben version

--- lib/geo.py
+++ lib/geo.py
@@ -10,7 +10,7 @@ class Position:
         self.lat, self.lon, self.alt = lat, lon, alt
 
     def distance(self, pos):
-        return haversine(self.lat, self.lon, pos.lat, pos.lon)
+        return haversine(self.lat, self.lon, pos.lat, pos.lon) * 1000
 
     def __str__(self):
         return '%3.5f %3.5f %4i' % (self.lat, self.lon, self.alt)
