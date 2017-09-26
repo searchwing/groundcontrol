7382b6a1face184c99fd0fcb9690d27004abc39b
sascha <sascha@searchwing.org>
2017-09-26 09:07:14 +0200

    next incarnation

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Activate virtualenv
-activate_this = '/home/pi/venv/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-
-import time
-
-from lib import ui
-from lib.gps import gps
-from lib.uav import uav
-from lib.switchboard import board
-    
+# @author: sascha@searchwing.org, August 2017
+"""Main.
+See notes for (how to install) requirements.
+See swgc.settings for settings.
+"""
 
 if __name__ == '__main__':
-    ui.start()
-    gps.start()
-    uav.start()
-    board.start()
 
+    # Activate python virtualenv in current user homedir.
+    import os
+    activate_this = '%s/venv/bin/activate_this.py' % os.path.expanduser('~')
+    execfile(activate_this, dict(__file__ = activate_this))
+
+    # Run app
+    from swgc import app
     try:
-        while 1:
-            time.sleep(1000)
+        app.run()
     except KeyboardInterrupt:
-        pass
-
+        # Intentional killed
+        print 'Ctrl-C'
+    
