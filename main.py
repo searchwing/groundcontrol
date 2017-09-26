f7e47ebc098e171b1474e8996c036aa27298ec0c
sascha <sascha@searchwing.org>
2017-09-26 09:29:36 +0200

    some housekeeping

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ See notes for (how to install) requirements.
 See swgc.settings for settings.
 """
 
+VENV_DIR = '/home/pi/venv/'
+
+
 if __name__ == '__main__':
 
-    # Activate python virtualenv in current user homedir.
-    import os
-    activate_this = '%s/venv/bin/activate_this.py' % os.path.expanduser('~')
-    execfile(activate_this, dict(__file__ = activate_this))
+    # Activate python virtualenv if any
+    if VENVDIR:
+        import os
+        activate_this = '%s/bin/activate_this.py' % VENV_DIR
+        execfile(activate_this, dict(__file__ = activate_this))
 
-    # Run app
+    # Run the app
     from swgc import app
-    try:
-        app.run()
-    except KeyboardInterrupt:
-        # Intentional killed
-        print 'Ctrl-C'
+    app.run()
     
