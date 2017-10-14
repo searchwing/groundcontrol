d80988c72e14c258adafc43375306b7ca8bafefe
sascha <sascha@searchwing.org>
2017-10-14 14:07:26 +0200

    better exit handling

--- swgc/app.py
+++ swgc/app.py
@@ -13,13 +13,19 @@ def run():
     """Call to get the app running.
     """
     uav.start(UAV_ADDRESS)
+    time.sleep(0.2)
     gps.start(GPS_PORT, GPS_BAUD)
+    time.sleep(0.2)
     board.start(BOARD_PORT, BOARD_BAUD)
+    time.sleep(0.2)
     ui.start()
 
     try:
         while 1:
             time.sleep(1000)
+    except KeyboardInterrupt:
+        print # Intentional exit
     except Exception, e:
         print e
-        sys.exit() # really release all resources
+    print 'Bye'
+    sys.exit() # Really release all resources
