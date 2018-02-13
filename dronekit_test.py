dbead58dbbf10645a1592027d2284201e4396a25
sascha <sascha@searchwing.org>
2018-02-13 17:47:57 +0100

    renamed

---
+++ dronekit_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Test.
+Test the swgc.uav module.
+"""
+
+
+VENV_DIR = None#'/Users/sascha/venv/'
+
+
+if __name__ == '__main__':
+
+    # Activate python virtualenv if any
+    if VENV_DIR:
+        activate_this = '%s/bin/activate_this.py' % VENV_DIR
+        execfile(activate_this, dict(__file__ = activate_this))
+
+    import sys
+    connection_string = sys.argv[1] if len(sys.argv) > 1 else None
+
+    # Run the uav test.
+    from swgc import uav_test
+    uav_test.run(connection_string = connection_string)
